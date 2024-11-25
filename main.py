81a8efab17f35c133d85083288281473993c4956
DivProC <divij712@gmail.com>
2024-11-25 11:02:50 +0530

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
 import heapq
-from collections import defaultdict, Counter
+from collections import Counter
 
-class Hufpoint:
+class HuffmanNode:
     def __init__(self, char=None, freq=0):
         self.char = char
         self.freq = freq
@@ -13,39 +12,40 @@ class Hufpoint:
         return self.freq < other.freq
 
 def calculate_frequencies(data):
-    freq = Counter(data)
-    return freq
+    return Counter(data)
 
-def build_tree(frequencies):
-    priority_queue = [Hufpoint(char, freq) for char, freq in frequencies.items()]
+def build_huffman_tree(frequencies):
+    priority_queue = [HuffmanNode(char, freq) for char, freq in frequencies.items()]
     heapq.heapify(priority_queue)
-    
+
     while len(priority_queue) > 1:
         left = heapq.heappop(priority_queue)
         right = heapq.heappop(priority_queue)
-        merged = Hufpoint(freq=left.freq + right.freq)
+        merged = HuffmanNode(freq=left.freq + right.freq)
         merged.left = left
         merged.right = right
         heapq.heappush(priority_queue, merged)
-    
+
     return priority_queue[0]
 
-def build_codes(node, code='', codes={}):
-    if node is None:
-        return
+def generate_huffman_codes(node, prefix='', codes=None):
+    if codes is None:
+        codes = {}
 
     if node.char is not None:
-        codes[node.char] = code
-    build_codes(node.left, code + '0', codes)
-    build_codes(node.right, code + '1', codes)
+        codes[node.char] = prefix
+    else:
+        generate_huffman_codes(node.left, prefix + '0', codes)
+        generate_huffman_codes(node.right, prefix + '1', codes)
 
     return codes
 
-def huffman_coding(data):
+def huffman_encode(data):
     frequencies = calculate_frequencies(data)
-    root = build_tree(frequencies)
-    codes = build_codes(root)
-    
+    root = build_huffman_tree(frequencies)
+    codes = generate_huffman_codes(root)
     encoded_data = ''.join(codes[char] for char in data)
-    return encoded_data
-  
+    return encoded_data, codes
+def huffman_coding(data):
+    return huffman_encode(data)[0]
+
